0269ffe8b9775866ed6c5cdaeaab7480c882397e
Brian Weasner <weasnerb@gmail.com>
2018-02-22 23:22:07 -0700

    Changed naming of user groups to user roles

--- sql/setupTables.sql
+++ sql/setupTables.sql
@@ -12,20 +12,20 @@ CREATE TABLE Users
 CREATE UNIQUE INDEX Users_email_uindex ON Users (email);
 
 
-# User Groups
-CREATE TABLE UserGroups
+# User Roles
+CREATE TABLE Roles
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
-CREATE UNIQUE INDEX Groups_name_uindex ON UserGroups (name);
+CREATE UNIQUE INDEX Roles_name_uindex ON Roles (name);
 
 
-# User UserGroup Cross Reference Table
-CREATE TABLE UserUserGroupsXRef
+# User Roles Cross Reference Table
+CREATE TABLE UsersRolesXRef
 (
     userId INT NOT NULL,
-    groupId INT NOT NULL,
+    roleId INT NOT NULL,
     CONSTRAINT User_fk FOREIGN KEY (userId) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT Group_fk FOREIGN KEY (groupId) REFERENCES UserGroups (id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT Roles_fk FOREIGN KEY (roleId) REFERENCES Roles (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
